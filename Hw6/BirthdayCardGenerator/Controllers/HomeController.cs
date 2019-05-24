cfa8d9cf132fac836aa1b9a0d7a2d188fa0032b1
Micah Hoffmann <micah.j.hoffmann@boeing.com>
2019-05-23 18:08:00 -0700

    Upload HW6 bithday card generator

---
+++ Hw6/BirthdayCardGenerator/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCardGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CardGenerator()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CardGenerator(Models.CardAttributes cardAttributes)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("thanks", cardAttributes);
+            } else
+            {
+                return View();
+            }            
+        }
+    }
+}
---
+++ Hw6/BirthdayCardGenerator/Models/CardAttributes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayCardGenerator.Models
+{
+    public class CardAttributes
+    {
+        [Required(ErrorMessage = "Please Enter From Name")]
+        public string From { get; set; }
+        [Required(ErrorMessage = "Please Enter To Name")]
+        public string To { get; set; }
+        [Required(ErrorMessage = "Please Enter Message")]
+        public string Message { get; set; }
+
+    }
+}
